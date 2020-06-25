269e58f7ddacc0c1e54c1f9bcddc5b7a9a7d8c8c
Matthew Gleich <matthewgleich@gmail.com>
2020-06-25 10:12:27 -0400

    ✨ Update home route
    Signed-off-by: Matthew Gleich <matthewgleich@gmail.com>

--- lib/routes/home/home.dart
+++ lib/routes/home/home.dart
@@ -1,5 +1,6 @@
 // 🐦 Flutter imports:
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class HomeRoute extends StatelessWidget {
   static const routeName = '/home';
@@ -8,8 +9,57 @@ class HomeRoute extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text(
-          'Hello World!',
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SvgPicture.asset(
+              'assets/images/logo.svg',
+              height: 200,
+              width: 200,
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            const Text(
+              'Welcome',
+              style: TextStyle(
+                fontSize: 40,
+              ),
+            ),
+            const Text(
+              'to the future',
+              style: TextStyle(
+                fontSize: 50,
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            RaisedButton(
+              onPressed: () => print('Pressed the button!'),
+              color: Theme.of(context).primaryColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  const Text(
+                    'Get Started',
+                    style: TextStyle(
+                      fontSize: 30,
+                    ),
+                  ),
+                  const Icon(
+                    Icons.chevron_right,
+                    size: 40,
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
--- lib/themes.dart
+++ lib/themes.dart
@@ -4,15 +4,18 @@ import 'package:flutter/material.dart';
 class Themes {
   // Shared values
   static const _primaryColor = Color.fromRGBO(104, 117, 245, 1);
+  static const _fontFamily = 'Nunito Sans';
 
   ThemeData get darkTheme => ThemeData(
         brightness: Brightness.dark,
         primaryColor: _primaryColor,
         scaffoldBackgroundColor: Color.fromRGBO(24, 26, 27, 1),
+        fontFamily: _fontFamily,
       );
 
   ThemeData get lightTheme => ThemeData(
         brightness: Brightness.light,
         primaryColor: _primaryColor,
+        fontFamily: _fontFamily,
       );
 }
